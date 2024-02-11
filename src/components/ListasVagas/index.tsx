225c35a3813d3e586ff85bbe7e90a4153551b739
LucasTymos <lucastymos@gmail.com>
2024-02-11 04:06:51 -0300

    feat: criando component ListaVagas

---
+++ src/components/ListasVagas/index.tsx
@@ -0,0 +1,42 @@
+import { iVagas } from "@/data/@types/iVagas";
+import styled from "styled-components";
+import VagaCard from "@/components/VagaCard";
+
+const ContainerVagas = styled.div`
+  max-width: 1100px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`;
+
+interface ListaVagasProps {
+  vagas: iVagas[];
+}
+
+function ListaVagas({ vagas }: ListaVagasProps) {
+  return (
+    <ContainerVagas>
+      {vagas.map((vaga) => (
+        <VagaCard
+          key={vaga.id}
+          id={vaga.id}
+          company={vaga.company}
+          logo={vaga.logo}
+          novo={vaga.novo}
+          featured={vaga.featured}
+          position={vaga.position}
+          role={vaga.role}
+          level={vaga.level}
+          postedAt={vaga.postedAt}
+          contract={vaga.contract}
+          location={vaga.location}
+          languages={vaga.languages}
+          tools={vaga.tools}
+        />
+      ))}
+    </ContainerVagas>
+  );
+}
+
+export default ListaVagas;
