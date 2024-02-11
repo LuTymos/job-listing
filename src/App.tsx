8a916daf82638094d6fffd99fbbcdfdc4f3c46ba
LucasTymos <lucastymos@gmail.com>
2024-02-11 03:28:37 -0300

    feat: aplicando estilo GLobal e criando HeaderBanner

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import styled from "styled-components";
+import GlobalStyles from "@/components/GlobalStyles";
+import bgHeaderDesktop from "@/assets/images/bg-header-desktop.svg";
+import bgHeaderMobile from "@/assets/images/bg-header-mobile.svg";
+
+const HeaderBannerContainer = styled.div`
+  width: 100%;
+  height: 156px;
+  background-color: #5ca5a5;
+  background-image: url(${bgHeaderDesktop});
+  background-repeat: no-repeat;
+  background-size: cover;
+  @media screen and (max-width: 768px) {
+    background-image: url(${bgHeaderMobile});
+  }
+`;
+
+const Background = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: var(--LightGrayishCyan);
+`;
+
 function App() {
-  return <></>;
+  return (
+    <Background>
+      <GlobalStyles />
+      <HeaderBannerContainer />
+    </Background>
+  );
 }
 
 export default App;
