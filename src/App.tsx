af6262b19a983145a8fe7951a9a129ac53fda88e
LucasTymos <lucastymos@gmail.com>
2024-03-31 20:35:22 -0300

    style: arrumando imagem do header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import styled from "styled-components";
 import GlobalStyles from "@/components/GlobalStyles";
-import bgHeaderDesktop from "@/assets/images/bg-header-desktop.svg";
-import bgHeaderMobile from "@/assets/images/bg-header-mobile.svg";
 import vagas from "@/data/services/data.json";
 import ListaVagas from "@/components/ListasVagas";
 import { FilterContextProvider } from "./data/context/FilterContext";
@@ -11,11 +9,11 @@ const HeaderBannerContainer = styled.div`
   width: 100%;
   height: 156px;
   background-color: #5ca5a5;
-  background-image: url(${bgHeaderDesktop});
+  background-image: url(https://job-listing-fawn.vercel.app/images/bg-header-desktop.svg);
   background-repeat: no-repeat;
   background-size: cover;
   @media screen and (max-width: 768px) {
-    background-image: url(${bgHeaderMobile});
+    background-image: url(https://job-listing-fawn.vercel.app/images/bg-header-mobile.svg);
   }
 `;
 
